bbbd930af01f5b0a04ab5e7d548301e292035e33
Ankit <ankit60.av@gmail.com>
2024-07-02 23:44:35 +0530

    added pt-6

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ import { BadgeCheckIcon } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-slate-800">
+    <div className="min-h-screen bg-slate-800 pt-6">
       <div className="p-4 container max-w-2xl space-y-4">
         <h1 className="text-3xl font-bold text-white">Comment Analysis Tool</h1>
 
