34956348f449b8f6d37b2f723c71899ddfe65099
Ankit <ankit60.av@gmail.com>
2024-07-03 23:22:15 +0530

    added senti ai api call

--- src/components/InputCard.tsx
+++ src/components/InputCard.tsx
@@ -6,11 +6,26 @@ import { Input } from "./ui/input";
 const InputCard = () => {
   const [url, setUrl] = useState("");
 
-  const handleUrlCall = () => {
+  const handleUrlCall = async () => {
     if (url.includes("youtube")) {
       const videoId = url.split("v=")[1];
 
       console.log("videoId ==> ", videoId);
+
+      const YOUTUBE_URL = "https://senti-ai-backend.onrender.com";
+      //   const YOUTUBE_URL = "http://localhost:8000";
+      try {
+        const finalUrl = YOUTUBE_URL + "/analyse/youtube/" + videoId;
+        console.log("Final url ==> ", finalUrl);
+        const response = await fetch(finalUrl);
+        if (!response.ok) {
+          throw new Error(`Response status: ${response.status}`);
+        }
+        const jsonRes = await response.json();
+        console.log("JSON RES ==> ", jsonRes);
+      } catch (error: any) {
+        console.error("error while calling api ==> ", error.message);
+      }
     }
   };
 
